4b0c7ef58af5616eed2558c332c48aa1ac1b6987
suyiming <suyiming3333@gmail.com>
2021-01-22 23:34:06 +0800

    security login return jtw token

--- auth-center/src/main/java/com/corn/springcloud/authcenter/security/config/WebSecurityConfig.java
+++ auth-center/src/main/java/com/corn/springcloud/authcenter/security/config/WebSecurityConfig.java
@@ -1,5 +1,8 @@
 package com.corn.springcloud.authcenter.security.config;
 
+import com.corn.springcloud.authcenter.security.filter.MyAuthenticationEntryPoint;
+import com.corn.springcloud.authcenter.security.filter.MyAuthenticationFilter;
+import com.corn.springcloud.authcenter.security.filter.MyJwtAuthorizationFilter;
 import com.corn.springcloud.authcenter.security.handler.FailureHandler;
 import com.corn.springcloud.authcenter.security.handler.LogoutHandler;
 import com.corn.springcloud.authcenter.security.handler.SuccessHandler;
@@ -10,6 +13,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -42,11 +46,23 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().formLogin()
-                .loginProcessingUrl("/login").permitAll()
+//                .loginProcessingUrl("/login").permitAll()
                 .successHandler(successHandler).permitAll()
                 .failureHandler(failureHandler).permitAll().and()
                 .logout().logoutSuccessHandler(logoutHandler).and()
                 .authorizeRequests()
-                .antMatchers("/**").permitAll();
+                .antMatchers("/oauth/**","/api/login").permitAll()
+                .anyRequest().authenticated()/**所有请求都需要认证才能访问**/
+                .and()
+                .addFilter(new MyAuthenticationFilter(authenticationManager()))/**设置身份认证过滤器**/
+                .addFilter(new MyJwtAuthorizationFilter(authenticationManager()))/**设置请求鉴权过滤器**/
+                .sessionManagement()
+                // 设置Session的创建策略为：Spring Security永不创建HttpSession 不使用HttpSession来获取SecurityContext
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                // 异常处理
+                .exceptionHandling()
+                // 匿名用户访问无权限资源时的异常
+                .authenticationEntryPoint(new MyAuthenticationEntryPoint());
     }
 }
---
+++ auth-center/src/main/java/com/corn/springcloud/authcenter/security/filter/MyAuthenticationFilter.java
@@ -0,0 +1,123 @@
+package com.corn.springcloud.authcenter.security.filter;
+
+import cn.hutool.json.JSONUtil;
+import com.corn.springcloud.authcenter.security.jwt.JwtTokenUtil;
+import org.springframework.security.authentication.*;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collection;
+
+/**
+ * 身份认证过滤器，用于处理登录逻辑
+ */
+public class MyAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private AuthenticationManager authenticationManager;
+
+    public MyAuthenticationFilter(AuthenticationManager authenticationManager){
+        this.authenticationManager = authenticationManager;
+        // 设置登录URL
+        super.setFilterProcessesUrl("/api/login");
+    }
+
+    /**
+     * 处理登录的逻辑
+     * @param request
+     * @param response
+     * @return
+     * @throws AuthenticationException
+     */
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        // 从输入流中获取到登录的信息
+        // 创建一个token并调用authenticationManager.authenticate() 让Spring security进行验证
+        //类似于subject.login(usernamePasswordToken);
+        return authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getParameter("username"),request.getParameter("password")));
+    }
+
+    /**
+     * 登录成功的回调,返回jwttoken
+     * @param request
+     * @param response
+     * @param chain
+     * @param authResult
+     * @throws IOException
+     */
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException {
+        User user= (User) authResult.getPrincipal();
+
+        // 从User中获取权限信息
+        Collection<? extends GrantedAuthority> authorities = user.getAuthorities();
+        // 创建Token
+        String token = JwtTokenUtil.createToken(user.getUsername(), authorities.toString());
+
+        // 设置编码 防止乱码问题
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json; charset=utf-8");
+        // 在请求头里返回创建成功的token
+        // 设置请求头为带有"Bearer "前缀的token字符串
+        response.setHeader("token", JwtTokenUtil.TOKEN_PREFIX + token);
+
+        // 处理编码方式 防止中文乱码
+        response.setContentType("text/json;charset=utf-8");
+        // 将反馈塞到HttpServletResponse中返回给前台
+        response.getWriter().write(JSONUtil.formatJsonStr("登录成功"));
+    }
+
+    /***
+     * 登录失败的回调
+     * @param request
+     * @param response
+     * @param failed
+     * @throws IOException
+     * @throws ServletException
+     */
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
+        String returnData="";
+        // 账号过期
+        if (failed instanceof AccountExpiredException) {
+            returnData="账号过期";
+        }
+        // 密码错误
+        else if (failed instanceof BadCredentialsException) {
+            returnData="密码错误";
+        }
+        // 密码过期
+        else if (failed instanceof CredentialsExpiredException) {
+            returnData="密码过期";
+        }
+        // 账号不可用
+        else if (failed instanceof DisabledException) {
+            returnData="账号不可用";
+        }
+        //账号锁定
+        else if (failed instanceof LockedException) {
+            returnData="账号锁定";
+        }
+        // 用户不存在
+        else if (failed instanceof InternalAuthenticationServiceException) {
+            returnData="用户不存在";
+        }
+        // 其他错误
+        else{
+            returnData="未知异常";
+        }
+
+        // 处理编码方式 防止中文乱码
+        response.setContentType("text/json;charset=utf-8");
+        // 将反馈塞到HttpServletResponse中返回给前台
+        response.getWriter().write(JSONUtil.formatJsonStr(returnData));
+    }
+
+}
