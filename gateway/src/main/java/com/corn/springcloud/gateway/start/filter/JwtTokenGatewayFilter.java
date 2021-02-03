44168d19778ff077a731295c06c2c1505baff5de
suyiming <suyiming3333@gmail.com>
2021-02-03 23:57:36 +0800

    feature:gateway filter支持oauth accesstoken 和security jwttoken 校验

---
+++ gateway/src/main/java/com/corn/springcloud/gateway/start/filter/JwtTokenGatewayFilter.java
@@ -0,0 +1,71 @@
+package com.corn.springcloud.gateway.start.filter;
+
+import com.alibaba.fastjson.JSONObject;
+import com.corn.springcloud.gateway.start.jwt.JwtTokenUtil;
+import io.jsonwebtoken.Claims;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.util.StringUtils;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.nio.charset.StandardCharsets;
+
+
+@Component
+public class JwtTokenGatewayFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String requestUrl = exchange.getRequest().getPath().value();
+        AntPathMatcher pathMatcher = new AntPathMatcher();
+        //api开头的进行jwttoken验证
+        if (pathMatcher.match("/api/**", requestUrl)) {
+            String tokenStr = exchange.getRequest().getHeaders().getFirst("Authorization");
+            if(StringUtils.isEmpty(tokenStr)){
+                return noTokenMono(exchange);
+            }else{
+                String token = tokenStr.split(" ")[1];
+                Claims claims = JwtTokenUtil.checkJWT(token);
+                if(claims == null){
+                    return invalidTokenMono(exchange);
+                }
+            }
+        }
+        return chain.filter(exchange);
+    }
+
+    private Mono<Void> invalidTokenMono(ServerWebExchange exchange) {
+        JSONObject json = new JSONObject();
+        json.put("status", HttpStatus.UNAUTHORIZED.value());
+        json.put("data", "无效的token");
+        return buildReturnMono(json, exchange);
+    }
+
+    private Mono<Void> noTokenMono(ServerWebExchange exchange) {
+        JSONObject json = new JSONObject();
+        json.put("status", HttpStatus.UNAUTHORIZED.value());
+        json.put("data", "没有token");
+        return buildReturnMono(json, exchange);
+    }
+
+    private Mono<Void> buildReturnMono(JSONObject json, ServerWebExchange exchange) {
+        ServerHttpResponse response = exchange.getResponse();
+        byte[] bits = json.toJSONString().getBytes(StandardCharsets.UTF_8);
+        DataBuffer buffer = response.bufferFactory().wrap(bits);
+        response.setStatusCode(HttpStatus.UNAUTHORIZED);
+        //指定编码，否则在浏览器中会中文乱码
+        response.getHeaders().add("Content-Type", "text/plain;charset=UTF-8");
+        return response.writeWith(Mono.just(buffer));
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
---
+++ gateway/src/main/java/com/corn/springcloud/gateway/start/jwt/JwtTokenUtil.java
@@ -0,0 +1,92 @@
+package com.corn.springcloud.gateway.start.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class JwtTokenUtil {
+    // Token请求头
+    public static final String TOKEN_HEADER = "Authorization";
+    // Token前缀
+    public static final String TOKEN_PREFIX = "Bearer ";
+
+    // 签名主题
+    public static final String SUBJECT = "piconjo";
+    // 过期时间
+    public static final long EXPIRITION = 1000 * 24 * 60 * 60 * 7;
+    // 应用密钥
+    public static final String APPSECRET_KEY = "aaabbbcccdddeeefffggghhhiiijjjkkklllmmmnnnooopppqqqrrrsssttt";
+    // 角色权限声明
+    private static final String ROLE_CLAIMS = "role";
+
+    /**
+     * 生成Token
+     */
+    public static String createToken(String username,String role) {
+        Map<String,Object> map = new HashMap<>();
+        map.put(ROLE_CLAIMS, role);
+
+        byte[] keyBytes = APPSECRET_KEY.getBytes();
+        SecretKey key = Keys.hmacShaKeyFor(keyBytes);
+
+        String token = Jwts
+                .builder()
+                .setSubject(username)
+                .setClaims(map)
+                .claim("username",username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRITION))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+        return token;
+    }
+
+    /**
+     * 校验Token
+     */
+    public static Claims checkJWT(String token) {
+        try {
+            final Claims claims = Jwts.parser().setSigningKey(APPSECRET_KEY.getBytes()).parseClaimsJws(token).getBody();
+            return claims;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * 从Token中获取username
+     */
+    public static String getUsername(String token){
+        Claims claims = Jwts.parser().setSigningKey(APPSECRET_KEY.getBytes()).parseClaimsJws(token).getBody();
+        return claims.get("username").toString();
+    }
+
+    /**
+     * 从Token中获取用户角色
+     */
+    public static String getUserRole(String token){
+        Claims claims = Jwts.parser().setSigningKey(APPSECRET_KEY.getBytes()).parseClaimsJws(token).getBody();
+        return claims.get("role").toString();
+    }
+
+    /**
+     * 校验Token是否过期
+     */
+    public static boolean isExpiration(String token){
+        Claims claims = Jwts.parser().setSigningKey(APPSECRET_KEY.getBytes()).parseClaimsJws(token).getBody();
+        return claims.getExpiration().before(new Date());
+    }
+
+    public static void main(String[] args) {
+        String token = "eyJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiW3VzZXI6bWFuYWdlXSIsInVzZXJuYW1lIjoie1wiZnVsbG5hbWVcIjpcIuiLj-S6pumTrVwiLFwiaWRcIjoxLFwicGFzc3dvcmRcIjpcIiQyYSQxMCR3RkExNGxHS2pQdWZmQVN0OE8vY1BPeTF2Rm01YWdYemRaR0xNbi9uZW5waFlRUkhhVnlkdVwiLFwidXNlcm5hbWVcIjpcInN1eWltaW5nXCJ9IiwiaWF0IjoxNjEyMzYyNTk3LCJleHAiOjE2MTI5NjczOTd9.enLBeNRIvLNVLycMOox0CrQuRY7BcgYhMeHMxyNhQM0";
+        Claims claims = checkJWT(token);
+        System.out.println(1);
+    }
+}
